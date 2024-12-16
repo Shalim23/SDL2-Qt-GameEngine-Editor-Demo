e0e6cb09488753cde321ccdd69f38945db1a26e1
Dmytro <dvshalimov@gmail.com>
2024-12-16 18:38:40 +0100

    qt + editor

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,11 +1,17 @@
+#ifdef EDITOR
+#include <Editor/Editor.h>
+#else
 #include <Engine/Engine.h>
+#endif // EDITOR
+
 #include "Registry/ComponentsRegistry.h"
 #include "Registry/SystemsRegistry.h"
 
 MAIN()
 {
 #ifdef EDITOR
-
+    Editor editor{argc, argv};
+    return editor.run();
 #else
     Engine engine{};
     engine.init<DemoSystems, DemoComponents>();
--- gen_sln.py
+++ gen_sln.py
@@ -4,9 +4,10 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-o", "--open", action="store_true", help="Open solution")
+parser.add_argument("--qt_path", required=True, help="Path to Qt libraries")
 args = parser.parse_args()
 
-subprocess.check_call(["cmake", "-S", ".", "-B", "./bin"])
+subprocess.check_call(["cmake", f"-DCMAKE_PREFIX_PATH={args.qt_path}", "-S", ".", "-B", "./bin"])
 
 if args.open:
     subprocess.Popen(["start", "bin/SDL2-Qt-GameEngine-Editor-Demo.sln"], shell=True)
